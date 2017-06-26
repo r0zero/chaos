57173df496e3e102c6bae9d73cee3a4c5c52b717
kang.li <kang.li@ele.me>
2017-06-26 18:57:22 +0800

    test keyword

---
+++ ele.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#test by rozero
+
+def get_domain_list():
+    domain = "ele.me"
+    rajax_me = "rajax.me"
+    git = "git.elenet.me"
+    print "get domain %s %s %s" %(domain, rajax_me, git)
+
+if __name__ == '__main__':
+    get_domain_list()
